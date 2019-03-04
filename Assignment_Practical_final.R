cd427ef4be664eb0d217c280b3c738cf82aca8a4
Maria Galazoula <gy16mg@leeds.ac.uk>
2019-03-04 23:46:12 +0000

    final code

--- Assignment_Practical_final.R
+++ Assignment_Practical_final.R
@@ -22,9 +22,6 @@ head(pl)
 head(sl)
 head(tr)
 
-levels(pl$commodity)
-?group_by
-?summarise
 #common fields
 #cl, pl, tr <- upc
 #tr, sl <- store
@@ -54,17 +51,24 @@ tr %>%
 #the people who bought "pasta"(commodity) in general
 
 #divide by length of each table, which will be the unique households in each case
-n2 <- length(n.hhpen$household)*100/length(unique(n.hhpen2$household))
+hh_pen <- length(n.hhpen$household)*100/length(unique(n.hhpen2$household))
 
 #Therefore, the answer to the first question is 22.065% or 22.07%
 
-label<- "Percentage"
-graph<- data.frame(Percentage= 22.07, Household_penetration = label)
 
+Household_penetration<-"Household penetration"
+Percentage<- round(hh_pen,2)
+graph<- data.frame(Household_penetration, Percentage)
 head(graph)
 
-ggplot(data=graph, aes(x=Household_penetration, y= Percentage))+
-  geom_bar(stat="identity", width = 0.5, fill= "steelblue")
+ggplot(graph, mapping=aes(Household_penetration, Percentage, label="22.07%"))+
+  geom_bar(stat="identity", width = 0.30, fill= "steelblue")+
+  theme(axis.text.x=element_blank())+
+  coord_cartesian(ylim = c(0, 25))+
+  geom_text(size = 3, position = position_stack(vjust = 0.5))
+
+
+
 
 #Q2: How does the household penetration of the product 
 #PRIVATE LABEL THIN SPAGHETTI vary within the two regions, 
@@ -72,28 +76,68 @@ ggplot(data=graph, aes(x=Household_penetration, y= Percentage))+
 
 #Again, begin with the transaction file
 tr %>%
-  left_join(pl) %>% #join the files of transaction and product look up
-  select("upc", "geography", "household","basket", "product_description")%>% #keep the variables that are of our interest
-  filter(product_description == "PRIVATE LABEL THIN SPAGHETTI") -> label #save it in a table
+  #join the files of transaction and product look up
+  left_join(pl) %>% 
+  #keep the variables that are of our interest
+  select("geography", "household", "product_description")%>% 
+  #save it in a table
+  filter(product_description == "PRIVATE LABEL THIN SPAGHETTI")-> private_label
 
-#View the top observations
-head(label)
+length(unique(private_label$household))
+
+private_label%>%#filter for geography 1
+  filter(geography==1)%>%
+  count(household)->private_label_1
+
+length(unique(private_label_1$household))
+
+private_label%>%#filter for geography 2
+  filter(geography==2)%>%
+  count(household)->private_label_2
+
+length(unique(private_label_2$household))
+
 
 #the same for the commodity pasta
 tr %>%
   left_join(pl) %>%
-  dplyr::select("upc", "geography", "household","basket", "product_description", "commodity")%>%
+  dplyr::select("upc", "geography", "household", "product_description", "commodity")%>%
   filter(commodity == "pasta")-> pasta_gen #save it in a table
+length(unique(pasta_gen$household))
+
+pasta_gen%>% #filter for geography 1
+  filter(geography==1)%>%
+  count(household)->pasta_gen_1
+
+length(unique(pasta_gen_1$household))
+
+pasta_gen%>%#filter for geography 2
+  filter(geography==2)%>%
+  count(household)->pasta_gen_2 
+
+length(unique(pasta_gen_2$household))
+
+hh_pen_1<-length(unique(private_label_1$household))*100/length(unique(pasta_gen_1$household))
+hh_pen_2<-length(unique(private_label_2$household))*100/length(unique(pasta_gen_2$household))
 
 
-  
+Regions<- c("Region 1", "Region 2")
+Percentage<-c(round(hh_pen_1,2), round(hh_pen_2,2) ) 
+graph<- data.frame(Regions, Percentage)
+head(graph)
+
+ggplot(graph, mapping=aes(Regions, Percentage, label=c("18.32%", "27.01%")))+
+  geom_bar(stat="identity", width = 0.30, fill= c("steelblue","deeppink3"))+
+  coord_cartesian(ylim = c(0, 30))+
+  geom_text(size = 3, position = position_stack(vjust = 0.5))
+
+
+
+
+
+
 #Q3: For which category of products (pasta, pasta sauces, syrups and pancake mixes) does the 
-#provision of coupons appear to have a positive impact? For this you should consider which customers first
-# purchased an item in a category using a coupon, and then how many of these customers made 
-#additional purchases of the item in the category. (30 marks) 
-
-##we can create a function for these commodities
-##first we will calculate everything for pasta
+#provision of coupons appear to have a positive impact?
 
 #find the households that bought with a coupon
 impact<-function(commod){
@@ -106,20 +150,15 @@ impact<-function(commod){
   
   #find number of households that bought a product with a coupon
   n.hh<- unique(r1$household)
-  # View(n.hh)
-  
   n.hh<- as.data.frame(n.hh)
-  # View(n.hh)
 
   #examine each household particularly 
   tr%>%
     left_join(pl)%>% #join the data 
     dplyr::select("upc",  "household","day", "product_description", "commodity","coupon")%>%
-    filter(commodity == commod)%>%
-    arrange(day, -coupon)->r2 #filter for the commodity that we have and save
-  
-  # View(r2)
-  
+    filter(commodity == commod)%>%#filter for the commodity that we have and save
+    arrange(day, -coupon)->r2 
+
   #create a function
   
   r<-vector("numeric")
@@ -133,27 +172,29 @@ impact<-function(commod){
     if (tmp[1]== 1 & length(tmp)>1)
       r<- append(r,n.hh[row,1])
   }
-  
-  # View(r)
-  
-  # length(unique(r))
-  
-  # length(unique(r1$household))
-  
+
   n5<-length(r)*100/length(unique(r1$household))
   print(n5)
 }
 
-
-impact("pasta")
-impact("pasta sauce")
-impact("pancake mixes")
-impact("syrups")
-
+impact_pasta<- impact("pasta")
+impact_pasta_sauce<-impact("pasta sauce")
+impact_pancake_mixes<-impact("pancake mixes")
+impact_syrups<-impact("syrups")
 
 
+Commodity<- c("Pasta", "Pasta sauce", "Pancake mixes", "Syrups")
+Percentage<-c(25.34, 29.53, 27.34, 26.74) 
+graph<- data.frame(Commodity, Percentage)
+head(graph)
 
+ggplot(graph, mapping=aes(Commodity, Percentage, label=c("25.34%", "29.53%", "27.34%", "26.74%")))+
+  geom_bar(stat="identity", width = 0.50, fill= c("cadetblue","cadetblue1", "cadetblue2", "cadetblue4"))+
+  coord_cartesian(ylim = c(0, 30))+
+  geom_text(size = 3, position = position_stack(vjust = 0.5))+
+  ggtitle("Impact of coupons on each commodity", position_dodge(middle)) 
 
+?ggtitle
 
 
 
